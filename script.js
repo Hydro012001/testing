141bb656b402bf38c7af212695885bb13f80e2f0
Hydro012001 <76999604+Hydro012001@users.noreply.github.com>
2022-10-28 13:39:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var menu = document.getElementById('menu-icon');
+
+    
+    
+
+function iconClick(){
+
+        var iconClick = document.getElementById('list');
+        if( iconClick.style.left === "0px"){
+                iconClick.style.left = "-100%"
+        }
+        else{
+                iconClick.style.left = "0px"
+        }
+       
+       
+    
+}
+
